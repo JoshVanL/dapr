9f1a64bc90b6d2c4752caf5b3fdcf3f222db40d6
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2023-06-30 21:19:45 +0000

    💄
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>

--- pkg/runtime/runtime.go
+++ pkg/runtime/runtime.go
@@ -2979,7 +2979,7 @@ func (a *DaprRuntime) establishSecurity(sentryAddress string) error {
 	if sentryAddress == "" {
 		return errors.New("sentryAddress cannot be empty")
 	}
-	log.Info("mTLS enabled. creating sidecar authenticator")
+	log.Info("mTLS enabled; Creating sidecar authenticator")
 
 	auth, err := security.GetSidecarAuthenticator(sentryAddress, a.runtimeConfig.certChain)
 	if err != nil {
@@ -2988,7 +2988,7 @@ func (a *DaprRuntime) establishSecurity(sentryAddress string) error {
 	a.authenticator = auth
 	a.grpc.SetAuthenticator(auth)
 
-	log.Info("authenticator created")
+	log.Info("Authenticator created")
 
 	diag.DefaultMonitoring.MTLSInitCompleted()
 	return nil
