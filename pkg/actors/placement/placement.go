c9c2c763fdd470aaa0ec1963e1fd9512dfeefa4e
joshvanl <me@joshvanl.dev>
2023-08-07 17:03:41 +0100

    Address review comments
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- pkg/actors/placement/placement.go
+++ pkg/actors/placement/placement.go
@@ -142,7 +142,7 @@ func (p *actorPlacement) AddHostedActorType(actorType string) error {
 
 // Start connects placement service to register to membership and send heartbeat
 // to report the current member status periodically.
-func (p *actorPlacement) Start(ctx context.Context) {
+func (p *actorPlacement) Start(ctx context.Context) error {
 	p.serverIndex.Store(0)
 	p.shutdown.Store(false)
 
@@ -241,6 +241,8 @@ func (p *actorPlacement) Start(ctx context.Context) {
 			}
 		}
 	}()
+
+	return nil
 }
 
 // Closes shuts down server stream gracefully.
