eca729ffc40a0c5025f0a12a7fa2265e6cd6b61e
joshvanl <me@joshvanl.dev>
2024-02-20 16:37:27 +0000

    Adds operator informer v1 and v2 tests
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- tests/integration/suite/daprd/subscriptions/declarative/operator/v2alpha1/v2alpha1.go
+++ tests/integration/suite/daprd/subscriptions/declarative/operator/v2alpha1/v2alpha1.go
@@ -11,9 +11,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package declarative
+package v2alpha1
 
 import (
-	_ "github.com/dapr/dapr/tests/integration/suite/daprd/subscriptions/declarative/operator"
-	_ "github.com/dapr/dapr/tests/integration/suite/daprd/subscriptions/declarative/selfhosted"
+	_ "github.com/dapr/dapr/tests/integration/suite/daprd/subscriptions/declarative/operator/v2alpha1/informer"
 )
